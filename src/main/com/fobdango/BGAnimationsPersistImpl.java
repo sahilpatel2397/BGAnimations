a78892422435cb85560a3ec03fb3bb9ad086c97f
Danny Lee <danny@wallou.net>
2018-04-04 17:05:05 -0400

    Added skeleton code for getting user data

--- src/main/com/fobdango/BGAnimationsPersistImpl.java
+++ src/main/com/fobdango/BGAnimationsPersistImpl.java
@@ -5,4 +5,50 @@ import java.sql.SQLException;
 
 public class BGAnimationsPersistImpl {
 
+	/** Get data from the Users table **/
+
+	public static ResultSet addUser(User u) throws SQLException {
+
+	}
+
+	public static ResultSet updateUser(User u) throws SQLException {
+
+	}
+
+	public static ResultSet deleteUser(User u) throws SQLException {
+
+	}
+
+	public static ResultSet banUser(User u) throws SQLException {
+
+	}
+
+	public static ResultSet getUserFirstName(User u) throws SQLException {
+
+	}
+
+	public static ResultSet getUserLastName(User u) throws SQLException {
+
+	}
+
+	public static ResultSet getUserEmail(User u) throws SQLException {
+
+	}
+
+	public static ResultSet getUserAddress(User u) throws SQLException {
+
+	}
+
+	public static ResultSet getUserAvatarUrl(User u) throws SQLException {
+
+	}
+
+	public static ResultSet getUserId(User u) throws SQLException {
+
+	}
+
+	public staic ResultSet getUserBanStatus(User u) throws SQLException {
+
+	}
+
 }
