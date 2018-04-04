3d3a47f9ccd663ce1e60ce6b4ca07a73cdb366a3
Daniel <danny@wallou.net>
2018-04-04 22:32:34 +0000

    Created first function for database connection

--- src/main/com/fobdango/BGAnimationsPersistImpl.java
+++ src/main/com/fobdango/BGAnimationsPersistImpl.java
@@ -1,54 +1,68 @@
-import java.sql.ResultSet;
+import java.sql.User;
 import java.sql.SQLException;
-
-
 
 public class BGAnimationsPersistImpl {
 
 	/** Get data from the Users table **/
 
-	public static ResultSet addUser(User u) throws SQLException {
+	public static boolean addUser(User u) throws SQLException {
+		String query = "INSERT INTO users " + 
+		"(firstName, lastName, email, address, avatarUrl)" +
+		" VALUES ('" + u.getFirstName() + "', '" + u.getLastName() + "', '" + 
+		u.getEmail() + "', '" + u.getAddress() + "', '" + u.getAvatarUrl() +"');"
+		
+		int result = DBAccessInterface.create(query);
+		if (result == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static User updateUser(User u) throws SQLException {
 
 	}
 
-	public static ResultSet updateUser(User u) throws SQLException {
+	public static User deleteUser(User u) throws SQLException {
 
 	}
 
-	public static ResultSet deleteUser(User u) throws SQLException {
+	public static User banUser(User u) throws SQLException {
 
 	}
 
-	public static ResultSet banUser(User u) throws SQLException {
+	public static User getUserFirstName(User u) throws SQLException {
 
 	}
 
-	public static ResultSet getUserFirstName(User u) throws SQLException {
+	public static User getUserLastName(User u) throws SQLException {
 
 	}
 
-	public static ResultSet getUserLastName(User u) throws SQLException {
+	public static User getUserEmail(User u) throws SQLException {
 
 	}
 
-	public static ResultSet getUserEmail(User u) throws SQLException {
+	public static User getUserAddress(User u) throws SQLException {
 
 	}
 
-	public static ResultSet getUserAddress(User u) throws SQLException {
+	public static User getUserAvatarUrl(User u) throws SQLException {
 
 	}
 
-	public static ResultSet getUserAvatarUrl(User u) throws SQLException {
+	public static User getUserId(User u) throws SQLException {
 
 	}
 
-	public static ResultSet getUserId(User u) throws SQLException {
+	public staic User getUserBanStatus(User u) throws SQLException {
 
 	}
-
-	public staic ResultSet getUserBanStatus(User u) throws SQLException {
-
+	
+	public static Card getCardNo(User u, Card c){
+		
 	}
+	
+	public static Car
 
 }
