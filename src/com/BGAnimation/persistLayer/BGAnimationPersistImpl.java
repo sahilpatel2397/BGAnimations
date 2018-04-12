6cc61f707942c42b83d46b9d03a722beba33ddad
Danny Lee <danny@wallou.net>
2018-04-12 13:32:33 -0400

    Debug messages

--- src/com/BGAnimation/persistLayer/BGAnimationPersistImpl.java
+++ src/com/BGAnimation/persistLayer/BGAnimationPersistImpl.java
@@ -20,7 +20,7 @@ public class BGAnimationPersistImpl {
 		throws SQLException, RuntimeException, NoSuchAlgorithmException,
 		NoSuchProviderException {
 		
-		//String encryptedPassword = PasswordHandler.getSecurePassword(password);
+		String encryptedPassword = PasswordHandler.getSecurePassword(password);
 		String query = "SELECT password FROM user WHERE user.email=\""+email+"\";";
 		ResultSet rs = DBAccessInterface.retrieve(query);
 		
@@ -28,7 +28,9 @@ public class BGAnimationPersistImpl {
 			if (rs.getString("password").equals(password)) {
 				return true;
 			} else {
-				System.out.println("ENTERS!@@#");
+				System.out.println("The password that was provided is: " + password);
+				System.out.println("The encrypted version of ^ is: " + encryptedPassword);
+				System.out.println("The password stored in the database is: " + rs.getString("password"));
 				return false;
 			}
 		} else {
